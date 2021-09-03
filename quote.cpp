e50156d3fba148d19cf1270b0e51faa64e05f526
Caleb <51061101+calebrwalk5@users.noreply.github.com>
2021-09-03 01:15:44 -0400

    Add files via upload

--- quote.cpp
+++ quote.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 using namespace std;
 
-char *author[6] = {"Linus Torvalds", "Larry Page", "Alan Touring", "Ada Lovelace", "Steve Wozniak", "Luke Smith"};
+char *author[7] = {"Linus Torvalds", "Larry Page", "Alan Touring", "Ada Lovelace", "Steve Wozniak", "Luke Smith", "me"};
 char *intro[6] = {"all", "no", "every", "most", "a lot of", "few"};
 char *noun[6] = {"life", "prize", "yourself", "you", "struggle", "love"};
 char *adjective[10] = {"happy", "sad", "good", "bad", "exciting", "boring", "okay", "not okay", "slowly", "quickly"};
 char *verb[8] = {"love", "see", "feel", "are", "learn", "live", "die", "think"};
 
 int main() {
-	//int word1, word2, word3, word4, word5;
-	int word1 = rand()% 6;
-	int word2 = rand() % 6;
-	int word3 = rand() % 10;
-	int word4 = rand() % 8;
+	int word1 = rand()% 5;
+	int word2 = rand() % 5;
+	int word3 = rand() % 9;
+	int word4 = rand() % 7;
 	int word5 = rand() % 6;
-	cout << intro[word1] << " " << noun[word2] << " is " << adjective[word3] << " to " << verb[word4] << "\n- " << author[word5];
+	cout << intro[word1] << " " << noun[word2] << " is " << adjective[word3] << " to " << verb[word4] << "\n- " << author[word5] << "\n";
 	return 0;
 }
